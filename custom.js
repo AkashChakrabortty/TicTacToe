c7ee9bf7a4c0b209ec3d6c51db7207137528a8f1
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-10-14 04:35:06 +0600

    add function but few bug found

--- custom.js
+++ custom.js
@@ -4,10 +4,10 @@ STEP
 ---------
 
 STEP:1 = By defalut , 0 button show and x button hidden
-STEP:2 = After click button 0,user click any click,find the column.If already value found in the column, then show that value,if no value found,show user value.by this step confirm it that user double or more value do not show at a time
-STEP:3 =
-STEP:4 =
-STEP:5 =
+STEP:2 = After click button 0,user click any column,find the column.If already value found in the column, then show that value,if no value found,show user value.by this step confirm it that user double or more value do not show at a time
+STEP:3 = diclare a aray.and store user value where index = colum
+STEP:4 = check 3 column value same or not
+STEP:5 = if same then show
 
 
 */
@@ -24,6 +24,104 @@ const buttonX = (status) => {
 button0(true);
 buttonX(false);
 /* ---- STEP-1 END-------- */
+
+//array diclare to store user value
+const valueArray = [];
+
+// function:3
+const check3Column = () => {
+    const row1 = valueArray[0] + valueArray[1] + valueArray[2];
+    const row2 = valueArray[3] + valueArray[4] + valueArray[5];
+    const row3 = valueArray[6] + valueArray[7] + valueArray[8];
+    const column1 = valueArray[0] + valueArray[3] + valueArray[6];
+    const column2 = valueArray[1] + valueArray[4] + valueArray[7];
+    const column3 = valueArray[2] + valueArray[5] + valueArray[8];
+    const arrow1 = valueArray[0] + valueArray[4] + valueArray[8];
+    const arrow2 = valueArray[2] + valueArray[4] + valueArray[6];
+    
+    if(row1 === '000' || row1 === 'xxx'){
+
+       const col0 = document.getElementById(`c-0`);
+       const col1 = document.getElementById(`c-1`);
+       const col2 = document.getElementById(`c-2`);
+       
+       col0.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+       col1.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+       col2.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+    }
+    if(row2 === '000' || row2 === 'xxx'){
+
+        const col3 = document.getElementById(`c-3`);
+        const col4 = document.getElementById(`c-4`);
+        const col5 = document.getElementById(`c-5`);
+        
+        col3.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col4.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col5.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+     if(row3 === '000' || row3 === 'xxx'){
+
+        const col6 = document.getElementById(`c-6`);
+        const col7 = document.getElementById(`c-7`);
+        const col8 = document.getElementById(`c-8`);
+        
+        col6.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col7.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col8.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+     if(column1 === '000' || column1 === 'xxx'){
+
+        const col0 = document.getElementById(`c-0`);
+        const col3 = document.getElementById(`c-3`);
+        const col6 = document.getElementById(`c-6`);
+        
+        col0.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col3.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col6.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+     if(column2 === '000' || column2 === 'xxx'){
+
+        const col1 = document.getElementById(`c-1`);
+        const col4 = document.getElementById(`c-4`);
+        const col7 = document.getElementById(`c-7`);
+        
+        col1.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col4.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col7.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+     if(column3 === '000' || column3 === 'xxx'){
+
+        const col2 = document.getElementById(`c-2`);
+        const col5 = document.getElementById(`c-5`);
+        const col8 = document.getElementById(`c-8`);
+        
+        col2.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col5.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col8.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+     if(arrow1 === '000' || arrow1 === 'xxx'){
+        const col0 = document.getElementById(`c-0`);
+        const col4 = document.getElementById(`c-4`);
+        const col8 = document.getElementById(`c-8`);
+       
+        col0.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col4.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col8.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+     if(arrow2 === '000' || arrow2 === 'xxx'){
+
+        const col2 = document.getElementById(`c-2`);
+        const col4 = document.getElementById(`c-4`);
+        const col6 = document.getElementById(`c-6`);
+        
+        col2.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col4.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+        col6.setAttribute('style','background-color:#DE3163;display:flex; justify-content: center;align-items: center;font-size:40px;');
+     }
+    else{
+        return false;
+    }
+}
 
 // button-0 click event
 document.getElementById('button-0').addEventListener('click',
@@ -51,12 +149,15 @@ document.getElementById('button-0').addEventListener('click',
     let getId =  document.getElementById(columnId);
     if(click==1){
         getId.innerHTML = '0';
+        //store value into the array
+        valueArray[column] = '0';
+        // style value
+        document.getElementById(columnId).setAttribute('style','font-size:40px; color: #d400d4;display:flex; justify-content: center;align-items: center;');
     }
     else{
         columnValue ? `${  getId.innerHTML = `${columnValue}`}` : `${  getId.innerHTML = ''}`;
     }
-    // style value
-    document.getElementById(columnId).setAttribute('style','font-size:40px; color: #d400d4;display:flex; justify-content: center;align-items: center;');
+  
     // show button x
     buttonX(!false);
     button0(!true);
@@ -90,12 +191,16 @@ document.getElementById('button-x').addEventListener('click',
      let getId =  document.getElementById(columnId);
      if(click==1){
          getId.innerHTML = 'x';
+          //store value into the array
+            valueArray[column] = 'x';
+             // style value
+            document.getElementById(columnId).setAttribute('style','font-size:40px;color: chartreuse;display:flex; justify-content: center;align-items: center;');
+            check3Column();
      }
      else{
          columnValue ? `${  getId.innerHTML = `${columnValue}`}` : `${  getId.innerHTML = ''}`;
      }
-     // style value
-     document.getElementById(columnId).setAttribute('style','font-size:40px;color: chartreuse;display:flex; justify-content: center;align-items: center;');
+     
      // show button 0
      button0(true);
      buttonX(false);
